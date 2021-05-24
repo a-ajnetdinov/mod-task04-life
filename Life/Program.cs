ecf0dc54e63a2b977810dab3a0932db0c3c4c3a9
Anton Ajnetdinov <57314412+a-ajnetdinov@users.noreply.github.com>
2021-05-24 19:50:52 +0300

    Update Program.cs

--- Life/Program.cs
+++ Life/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using cli_life.config;
 
 namespace cli_life
 {
@@ -29,23 +30,48 @@ namespace cli_life
     {
         public readonly Cell[,] Cells;
         public readonly int CellSize;
+        public readonly Config config;
 
         public int Columns { get { return Cells.GetLength(0); } }
         public int Rows { get { return Cells.GetLength(1); } }
         public int Width { get { return Columns * CellSize; } }
         public int Height { get { return Rows * CellSize; } }
-
-        public Board(int width, int height, int cellSize, double liveDensity = .1)
-        {
-            CellSize = cellSize;
-
-            Cells = new Cell[width / cellSize, height / cellSize];
+        /// <summary>
+        /// Продолжается симуляция из файла
+        /// </summary>
+        /// <param name="sb"></param>
+        public Board(StateBoard sb) {
+            if (sb != null) {
+                config = sb.config;
+                Cells = sb.GetCell();
+                CellSize = config.cellSize;
+                ConnectNeighbors();
+            } else {
+                config = Config.DefaultConfig;
+                CellSize = config.cellSize;
+                Cells = new Cell[config.width / config.cellSize, config.height / config.cellSize];
+                CreateBoard();
+            }
+                
+		}
+        /// <summary>
+        /// Создается новая симуляция
+        /// </summary>
+        /// <param name="config"></param>
+        public Board(Config config) {
+            if (config == null)
+                config = Config.DefaultConfig;
+            this.config = config;
+            CellSize = config.cellSize;
+            Cells = new Cell[config.width / config.cellSize, config.height / config.cellSize];
+            CreateBoard();
+        }
+        private void CreateBoard() {
             for (int x = 0; x < Columns; x++)
                 for (int y = 0; y < Rows; y++)
                     Cells[x, y] = new Cell();
-
             ConnectNeighbors();
-            Randomize(liveDensity);
+            Randomize(config.liveDensity);
         }
 
         readonly Random rand = new Random();
@@ -86,16 +112,21 @@ namespace cli_life
             }
         }
     }
-    class Program
+    
+	class Program
     {
+        public enum State {
+            RUN,
+            MENU
+		}
+        public static Dictionary<string, MenuState> menuStates;
+        public static MenuState currentState;
+        public static State state;
         static Board board;
+        static Config config;
         static private void Reset()
         {
-            board = new Board(
-                width: 50,
-                height: 20,
-                cellSize: 1,
-                liveDensity: 0.5);
+            board = new Board(config);
         }
         static void Render()
         {
@@ -116,15 +147,54 @@ namespace cli_life
                 Console.Write('\n');
             }
         }
+        static void Pause(object sender, ConsoleCancelEventArgs args) {
+            state = State.MENU;
+            args.Cancel = true;
+		}
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine(currentState.text);
+            string key = Console.ReadLine();
+            currentState.actions[key](key);
+        }
+        public static void Save() {
+            Console.WriteLine("Введите путь для сохранения");
+            string path = Console.ReadLine();
+            StateBoard.SaveBoard(board, path);
+		}
+        public static void Load() {
+            Console.WriteLine("Введите путь загрузки");
+            string path = Console.ReadLine();
+            board = new Board(StateBoard.LoadBoard(path));
+            state = State.RUN;
+		}
         static void Main(string[] args)
         {
+           currentState = new MenuState(
+                "1 - Continue\n2 - Save\n3 - Load",
+                new Dictionary<string, Action<string>> {
+                    {"1",(str) =>  state = State.RUN},
+                    {"2",(str) => Save() },
+                    {"3",(str) => Load() }
+				}
+            );
+
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(Pause);
             Reset();
             while(true)
             {
-                Console.Clear();
-                Render();
-                board.Advance();
-                Thread.Sleep(1000);
+				switch (state) {
+                    case State.MENU:
+                        Menu();
+                        break;
+                    case State.RUN:
+                        Console.Clear();
+                        Render();
+                        board.Advance();
+                        Thread.Sleep(1000);
+                        break;
+				}
             }
         }
     }
